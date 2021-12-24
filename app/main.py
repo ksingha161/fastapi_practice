9d0f2eb0acb7e13065c88e175c7fca5056f7367f
ksingha161 <karanjeetahluwalia@gmail.com>
2021-12-24 14:58:25 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ import psycopg2
 from . import models
 from .database import engine, get_db
 from .routers import product, user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 app.include_router(
     product.router, 
